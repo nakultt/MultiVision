2f7fd249300f0ecb4546d02d4bb93373d27794b7
Nakultt <nakulnuked@gmail.com>
2025-05-04 01:16:11 +0530

    Refactor object detection module: move detect_obj function to vision.py and update import paths

--- multi_vision.py
+++ multi_vision.py
@@ -1,4 +1,4 @@
-from object_detection.multi_vision import detect_obj
+from object_detection.vision import detect_obj
 from face_detection.train_faces import train_model
 from face_detection.detect_faces import recognize_faces
 
